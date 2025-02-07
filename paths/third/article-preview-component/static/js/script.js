8f561cea4689a12b3a991a6f8d23215bd4785998
Ankit Gond <128490122+ankitashokgond@users.noreply.github.com>
2025-02-07 20:18:07 +0530

    Added support for the social media share

--- paths/third/article-preview-component/static/js/script.js
+++ paths/third/article-preview-component/static/js/script.js
@@ -1,5 +1,10 @@
-shareSection = document.querySelector('.share__section');
-shareBtn = document.querySelector('.share__btn');
+const shareSection = document.querySelector('.share__section');
+const shareBtn = document.querySelector('.share__btn');
+
+const currentUrl = encodeURIComponent(window.location.href);
+const facebookShare = document.querySelector('.facebook');
+const twitterShare = document.querySelector('.twitter');
+const pinterest = document.querySelector('.pinterest');
 
 shareBtn.addEventListener('click', function () {
   if (shareSection.classList.contains('hidden')) {
@@ -11,3 +16,7 @@ shareBtn.addEventListener('click', function () {
   }
   
 })
+
+facebookShare.href = `https://www.facebook.com/sharer/sharer.php?u=${currentUrl}`;
+twitterShare.href = `https://twitter.com/intent/tweet?url=${currentUrl}`;
+pinterestShare.href = `https://www.pinterest.com/pin/create/button/?url=${currentUrl}`;
